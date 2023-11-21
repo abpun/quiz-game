bd6781b7ff31094774d3c094a5b4f729375f7763
Abhishek Pun <56480651+abpun@users.noreply.github.com>
2023-11-21 12:02:11 +0545

    Update Settings.js

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -70,7 +70,7 @@ export default function Home() {
                     render={(props) => (
                         <SettingsForm
                             {...props}
-                            options={["5", "10", "15"]}
+                            options={["5", "10", "20"]}
                             color="primary"
                             name="totalQuestion"
                             watch={watch}
