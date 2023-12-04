cf9ad48ccb4c4a97f99f21d67ccff03bc8686f00
Abhishek Pun <abhishek.pun.3@gmail.com>
2023-12-04 22:57:26 +0545

    bug fix add highscores

--- frontend/src/config/http.js
+++ frontend/src/config/http.js
@@ -1,15 +1,14 @@
-// axios instance
+import axios from "axios";
+import { loggedInData } from "./authData";
+
 const http = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/${process.env.REACT_APP_VERSION}`,
 });
 
-// Function to set Authorization header before each request
 const setAuthToken = (token) => {
   if (token) {
-    // Apply the token to the Authorization header
     http.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
-    // If no token is provided, remove Authorization header
     delete http.defaults.headers.common["Authorization"];
   }
 };
